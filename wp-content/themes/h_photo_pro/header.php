339bdc385b6f0c2cfd3d8d362ca8bdc6205c1aec
Oscar PALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-12-14 18:59:22 +0100

    add favicon

--- wp-content/themes/h_photo_pro/header.php
+++ wp-content/themes/h_photo_pro/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hadrien</title>
+    <link rel="icon" type="image/x-icon" href="<?= get_template_directory_uri() . './assets/img/logo.png' ?>">
+    <title>@h_photo_pro</title>
 </head>
 
 <body>
